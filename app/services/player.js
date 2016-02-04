1c67a1292192c277e5fe90ebbb5db9ac118107cd
creasetoph <chris.albert@ticketfly.com>
2016-02-03 20:43:21 -0800

    Creating a facade for audio

--- app/services/player.js
+++ app/services/player.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 import textFormatters from '../utils/text-formatters';
 
 var FakePlayer = Ember.Object.extend({
-  autoplay: false,
-  src: null,
+  autoplay   : false,
+  src        : null,
   currentTime: 0,
   play() {
     console.debug('Fake player play [' + this.get('src') + ']');
@@ -16,18 +16,50 @@ var FakePlayer = Ember.Object.extend({
   }
 });
 
+var HtmlPlayer = Ember.Object.extend({
+  autoplay: false,
+  src     : null,
+  audio  : null,
+  init() {
+    var audio = new Audio();
+    this.set('audio', audio);
+    audio.autoplay = true;
+  },
+  currentTime() {
+    return this.get('audio').currentTime;
+  },
+  setSrc(url) {
+    this.get('audio').src = url;
+  },
+  play() {
+    this.get('audio').play();
+  },
+  pause() {
+    this.get('audio').pause();
+  },
+  fastSeek(seekTo) {
+    //this.get('audio').fastSeek(seekTo);
+  },
+  setAutoPlay(ap) {
+    this.get('audio').autoplay = ap;
+  },
+  on(event, fn) {
+    this.get('audio').addEventListener(event, fn);
+  }
+});
+
 export default Ember.Service.extend({
-  playlist: Ember.inject.service('playlist'),
-  audio: null,
-  playing: false,
-  paused: false,
-  stopped: false,
-  currentTime: 0,
+  playlist    : Ember.inject.service('playlist'),
+  audio       : null,
+  playing     : false,
+  paused      : false,
+  stopped     : false,
+  currentTime : 0,
   timeInterval: null,
   init() {
-    this.set('audio',new Audio());
+    this.set('audio', HtmlPlayer.create());
     //this.set('audio',FakePlayer.create());
-    this.get('audio').addEventListener('ended',() => {
+    this.get('audio').on('ended', () => {
       this.audioEnded();
     });
   },
@@ -38,20 +70,20 @@ export default Ember.Service.extend({
   sourceChanged() {
     this.setProperties({
       playing: false,
-      paused: false,
+      paused : false,
       stopped: false
     });
     this.play();
   },
   play() {
     this.startUpdater();
-    if(this.get('paused')) {
+    if (this.get('paused')) {
       //This means we came from being paused, so just resume
       this.set('paused', false);
       this.get('audio').play();
-    }else if (this.get('stopped')) {
+    } else if (this.get('stopped')) {
       //This means we came from being stopped, so set seek to 0 and play
-      this.set('stopped',false);
+      this.set('stopped', false);
       this.get('audio').fastSeek(0);
       this.get('audio').play();
     } else {
@@ -59,26 +91,26 @@ export default Ember.Service.extend({
       this.get('playlist').getCurrentTrack()
         .then(t => {
           var audio = this.get('audio');
-          audio.autoplay = true;
-          audio.src = t.stream.url;
+          audio.setAutoPlay(true);
+          audio.setSrc(t.stream.url);
           this.set('playing', true);
         });
     }
   },
   pause() {
     this.stopUpdater();
-    this.set('paused',true);
+    this.set('paused', true);
     this.get('audio').pause();
   },
   stop() {
     this.stopUpdater();
-    this.set('stopped',true);
+    this.set('stopped', true);
     this.get('audio').pause();
   },
   startUpdater() {
     this.timeInterval = setInterval(() => {
-      this.set('currentTime',this.get('audio').currentTime * 1000);
-    },500);
+      this.set('currentTime', this.get('audio').currentTime() * 1000);
+    }, 500);
   },
   stopUpdater() {
     clearInterval(this.timeInterval);
