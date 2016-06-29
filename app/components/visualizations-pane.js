1b308b2d37fb9803c46ea0d4ac68a66b5feab484
creasetoph <chris.albert@ticketfly.com>
2016-06-29 15:43:13 -0700

    Adding viz pane

---
+++ app/components/visualizations-pane.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import config from 'cloud-amp/config/environment';
+
+export default Ember.Component.extend({
+  classNames: ['visualizations','row']
+});
---
+++ app/controllers/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  showMedia: true,
+  showViz: false,
+  actions: {
+    mediaClicked() {
+      this.set('showMedia',true);
+      this.set('showViz',false);
+    },
+    vizClicked() {
+      this.set('showMedia',false);
+      this.set('showViz',true);
+    }
+  }
+});
